8ddb0c2cfd73fcdd969d71631eedaf0f6ede6538
Alexandra Dinca <alexandradi2003@gmail.com>
2026-04-18 13:33:57 +0300

    Adding simple auth service skeleton to git.

---
+++ src/test/java/org/acs/idp/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acs.idp.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
